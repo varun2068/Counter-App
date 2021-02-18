60ba61fa53dcc30e3e8e664c3bab9f7a7be40875
Varun Gupta <58721404+varun2068@users.noreply.github.com>
2021-02-18 13:56:24 +0530

    Add files via upload

---
+++ counter.js
@@ -0,0 +1,54 @@
+var plusButton = document.getElementById("add");
+var minusButton = document.getElementById("sub");
+var reset = document.getElementById("re");
+// Counter Activator
+var counter = document.getElementById("count");
+
+// Function to increase count by one
+$(plusButton).on("click", function() {
+    counter++;
+   // Part to disable the button after 10
+    if(counter < 11){
+       plusButton.removeAttribute("disabled");
+    }
+    else{
+        plusButton.attributes('disabled', "disabled");
+    }
+    //Part to check counter is even or odd
+    if(counter % 2 == 0){
+        $("body").css("background-color", "rgb(124, 202, 253)");
+        $('p').css("color", "white");
+    }
+    else{
+        $("body").css("background", "grey");
+        $('p').css("color", "red");
+    }
+    $('p').text(counter);
+});
+ 
+// Function to reduce count by one
+$(minusButton).on("click", function() {
+    counter--;
+    // Part to disable the button after 0
+    if(counter < 0){
+        minusButton.attribute("disabled", "disabled");
+    }
+    else{
+        minusButton.removeAttribute("disabled");
+    }
+    //Part to check counter is even or odd
+    if(counter % 2 == 0){
+        $("body").css("background-color", "rgb(124, 202, 253)");
+        $('p').css("color", "white");
+    }
+    else{
+        $("body").css("background", "grey");
+        $('p').css("color", "red");
+    }
+    $('p').text(counter);
+});
+// Function to reset the button
+$(reset).on("click", function(){
+    counter = 0;
+    $('p').text(counter);
+})
